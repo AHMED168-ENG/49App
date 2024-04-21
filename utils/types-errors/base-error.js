67f5101efd094dce034bd1d896b95177c9f0ff33
Qutuz <seifeldinrefaat@gmail.com>
2024-04-21 09:41:33 +0200

    create NotFoundError

--- utils/types-errors/base-error.js
+++ utils/types-errors/base-error.js
@@ -1,6 +1,6 @@
 import httpStatus from "http-status";
 
-class BaseError extends Error {
+export default class BaseError extends Error {
   constructor(name, httpCode, message) {
     super(message);
     Object.setPrototypeOf(this, new.target.prototype);
